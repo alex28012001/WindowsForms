f4c587ede73cd86ee740cc2fde8b02d5f2cc7887
alex28012001 <star4enko.aleksey2015@yandex.ru>
2017-10-29 23:06:31 +0300

    Create Encryption.cs

---
+++ Encryption.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace AuthorizationForm.AuthorizationRepository
+{
+    static public class Repository 
+    {
+        //шифрование
+        static public string Encryption(string password)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
